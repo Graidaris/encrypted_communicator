d1de3d4530a4db36823d448ccffbafba034e4762
Graidaris <vladislavby96@gmail.com>
2020-03-27 00:40:40 +0300

    add comments, notes

--- communicator/communicator.py
+++ communicator/communicator.py
@@ -8,9 +8,13 @@ import sys
 
 
 class Communicator:
-    def __init__(self, port):
-        self.thread_catch_message = threading.Thread(target=self.__catch_messages)
-        self.thread_cms = threading.Thread(target=self.__catch_mfs) #catch message from server
+    """
+    
+    """
+    
+    def __init__(self, port: int):
+        
+        self.thread_cms = threading.Thread(target=self.__catch_mfs)  # catch message from server
         self.HOST_NAME = socket.gethostname()
         self.IP = socket.gethostbyname(self.HOST_NAME)
         self.PORT = port
@@ -19,101 +23,131 @@ class Communicator:
         self.socket_recv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
+        self.lock = threading.Lock()
+
         self.connectors = []
         self.text = ''
         print("Initial communicator")
-                
-    def print_text(self, text):
+        
+
+    def print_text(self, text: str) -> None:
         '''Print text in a console and add the text to the variable (self.text)'''
-        print(text)
-        self.text = self.text + text
-            
-    def received_text(self):
+        with self.lock:
+            print(text)
+            self.text = self.text + text
+
+    def received_text(self) -> str:
         '''Returns text that has been intercepted \n
             after calling the method, the stream is cleared
         '''
-        text = self.text
-        self.text = ''
+        with self.lock: # check pyqt5 thread lock 
+            text = self.text
+            self.text = ''
+        
         return text
     
-    def start_listen(self):
-        if self._stop: return
-        
-        self.print_text(f"start the server\t{self.IP}:{self.PORT} ")
-        self.stop = True
-        
-        self.socket_recv.bind((self.IP, self.PORT))
-        self.socket_recv.listen(1)       
-        
-        self.thread_catch_message.start()        
-    
-    def stop_listen(self):
+
+    def stop_listen(self) -> None:
         self._stop = True
         self.socket_recv.close()
         self.socket_send.close()
-        
-    def new_message(self):
+
+    def new_message(self) -> bool:
         '''Return True if the stream has new content, and False if if does't '''
-        
+
         if len(self.text) > 0:
             return True
         else:
             return False
-    
-    def __catch_messages(self):
+
+    def __catch_messages(self) -> None:
         while not self._stop:
             try:
                 conn, addr = self.socket_recv.accept()
             except OSError as e:
                 print(e)
                 return
-                            
+
             self.connectors.append(addr)
-            threading._start_new_thread(self.__processing_message, (conn, addr))
-            
-            
-    def __processing_message(self, conn, addr):
+            threading._start_new_thread(
+                self.__processing_message, 
+                (conn, addr)
+            )
+
+    def __processing_message(self, conn: socket, addr: str) -> None:
         while True:
             data = conn.recv(self.BUFFER_SIZE)
             if not data:
                 break
-            
+
             data = data.decode()
             self.print_text(str(addr) + ":\t" + str(data))
-                
+
         print(f"Connected user {addr} is closed")
         conn.close()
-        
-    def __catch_mfs(self):
+
+    def __catch_mfs(self) -> None:
         """Catch message from server"""
-        
+
         while not self._stop:
-            data = self.socket_send.recv(self.BUFFER_SIZE)
+            data = self.socket_recv.recv(self.BUFFER_SIZE)
             if not data:
                 break
+
+            threading._start_new_thread(
+                self.__processing_mfs, 
+                (data)
+            )
             
-            data = data.decode()
-            print(f"From server: {data}")
-    
-    def send_message(self, message):
-        self.socket_send.send(message.encode())
+    def __processing_mfs(self, data) -> None:
+        """Processing data from the server
+        Dictionary: {"FROM":from, "TO":to, "WTF":wtf}
+        wtf is a like enum File, Text or something else        
+        """
+        #Need something for identificate the kind of a message
+        #send readiness to the server like "U can send me the data"
+        #Client <---{dictionary with info of kind of the message}-- Server
+        #Client ----{"OK"}-> Server
+        #Client <---{data}-- Server
+        #Client ----{done thanks}-> Server
         
-    def connect(self, addres, port):
+        pass
+
+    def send_message(self, message: str, to: str) -> None:
+        """Send the message (text) to the client via server
+        
+        
+        """
+        #Client ---{dictionary with info of kind of the message}--> Server
+        #Client <---{"OK"}-- Server
+        #Client ----{data}-> Server
+        #Client <---{done thanks}- Server
+        pass
+
+    def connect(self, addres: str, port: int) -> None:
+        """Connect to the server
+        Not the recipient of the message
+        
+        """
         self.socket_send.connect((addres, port))
         self.thread_cms.start()
-    
-    
+        
+    def start_listen(self) -> None:
+        """Start listen port for receive data from the server.
+        
+        """
+        self.thread_cms.start()
+        
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         com = Communicator(int(sys.argv[1]))
-        
-        com.connect(com.IP,int(sys.argv[2]))
+
+        com.connect(com.IP, int(sys.argv[2]))
         message = None
         while message != "out":
             message = input("enter message: ")
-            com.send_message(message)
-    
-    
-    
-    com.stop_listen()
-    
+            #com.send_message(message)
+
+        com.stop_listen()
