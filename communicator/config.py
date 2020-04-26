ee2b046d663d80909c680de4589cdb993c9e3888
Graidaris <vladislavby96@gmail.com>
2020-04-26 15:33:46 +0300

    add module config.py

---
+++ communicator/config.py
@@ -0,0 +1,56 @@
+import os
+import json
+
+DEFAULT_CONFIGS = {"FOLDERS": {"LOGING": "Logs",
+                            "DOWNLOAD": "Downloads",
+                            "PRIVAT_KEY": "priv_key",
+                            "PUBLIC_KEYS": "pub_key"},
+                "SIZE_RSA_KEY": 3072,
+                "BUFFER_SIZE": 1024}
+
+DEFAULT_FILENAME = "config.json"
+
+
+class Config:
+    def __init__(self):
+        self.config = {}        
+
+    def load_config(self, path=DEFAULT_FILENAME) -> None:
+        """Load the config file.
+        Params:
+            param 1: path = path to the config, default path = DEFAULT_FILENAME
+        
+        """
+        
+        if os.path.exists(path):
+            with open(path, "rb") as f:
+                self.config = json.load(f)
+                
+    def integrity(self) -> None:
+        """Check the config for integrity."""
+        Config.check_integrity(self.config, DEFAULT_CONFIGS)
+        
+    def save_config(self, path=DEFAULT_FILENAME):
+        path, filename = os.path.split(path)
+        if path != "":
+            if not os.path.exists(path):
+                os.makedirs(path)
+            
+        with open(filename, 'w') as f:
+            json.dump(self.config, f)
+    
+    @staticmethod
+    def check_integrity(a:dict, b:dict) -> None:
+        """Check if everything necessary is present in the config. 
+        Comparing a and b dictionary.
+        
+        """
+        
+        for key_b in b:
+            if not key_b in a:
+                a[key_b] = b[key_b]                
+            elif type(b[key_b]) is dict:
+                Config.check_integrity(a[key_b], b[key_b])
+            elif type(a[key_b]) is not type(b[key_b]):
+                raise TypeError(f"type of key:{key_b} = {type(b[key_b])} and {type(a[key_b])}  ")
+    
